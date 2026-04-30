d89a034ba9c7408c3829adba454eca439e7fc7ce
julianlamaravilla <julianrestrepo012@gmail.com>
2026-04-30 18:01:17 -0500

    add a correction abput geenrate_image() method state

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         PremiumUser user = new PremiumUser();
         user.use_ai_prompt();
-        user.generate_image();
+        user.use_ai_prompt();
     }
 }
--- src/main/java/com/example/PremiumUser.java
+++ src/main/java/com/example/PremiumUser.java
@@ -3,7 +3,7 @@ package com.example;
 public class PremiumUser extends User {
     public int credits = 100;
 
-    public void generate_image() {
+    private void generate_image() {
         this.credits -= 5;
         System.out.println("Image generated successfully!");
     }
